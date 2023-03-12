048299a17bf7266e5651788d2b612f6be56a10e4
Виктор Ганзенко <viktorganzenko@MacBook-Pro-Viktor.local>
2023-03-12 21:02:50 +0200

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import { AppStyled } from './App.styled';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactsList from './ContactsList';
+
+const shortid = require('shortid');
+const contactId = shortid.generate(15);
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onSubmitHendler = data => {
+    const contact = {
+      id: contactId,
+      name: data.name,
+      number: data.number,
+    };
+    contact.id = shortid.generate();
+    const contactName = [];
+
+    for (const contact of this.state.contacts) {
+      contactName.push(contact.name);
+    }
+
+    if (contactName.includes(contact.name)) {
+      alert(`${contact.name} is already in contacts list`);
+      return true;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+    return false;
+  };
+
+  filterName = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  delete = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contactId !== contact.id),
+    }));
+  };
+
+  render() {
+    const filterNormilized = this.state.filter.toLowerCase().trim();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterNormilized)
+    );
+
+    return (
+      <AppStyled>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={this.onSubmitHendler} />
+        <Filter value={this.state.filter} onChengeFilter={this.filterName} />
+        <ContactsList contacts={visibleContacts} deleteContact={this.delete} />
+      </AppStyled>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,72 @@
+import { Component } from 'react';
+import { Form, Input, Button } from './ContactForm.styled';
+
+const shortid = require('shortid');
+const inputNameId = shortid.generate();
+const inputNumberId = shortid.generate();
+const buttonId = shortid.generate();
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handelInputChange = event => {
+    this.setState({ [event.target.name]: event.currentTarget.value });
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handelSubmit = event => {
+    event.preventDefault();
+    const isIncludes = this.props.onSubmitForm(this.state);
+    if (isIncludes) return;
+    this.reset();
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handelSubmit}>
+        <label htmlFor={inputNameId}>
+          <span>Name</span>
+        </label>
+        <Input
+          autoComplete="off"
+          type="text"
+          name="name"
+          id={inputNameId}
+          value={this.state.name}
+          onChange={this.handelInputChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+
+        <label htmlFor={inputNumberId}>
+          <span>Number</span>
+        </label>
+        <Input
+          type="tel"
+          name="number"
+          id={inputNumberId}
+          value={this.state.number}
+          onChange={this.handelInputChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+
+        <label htmlFor={buttonId}>
+          <Button type="submit" id={buttonId}>
+            Add contact
+          </Button>
+        </label>
+      </Form>
+    );
+  }
+}
+
+export default ContactForm;
